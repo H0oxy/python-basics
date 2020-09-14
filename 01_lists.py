cda2dd7517ffac4130cfa9a6863a9267b74c4c66
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-09-14 11:05:30 +0500

    sredniy ball studenta

--- 01_lists.py
+++ 01_lists.py
@@ -38,3 +38,20 @@ while True:
 
 print('Ввод завершен')
 print(student_marks)
+
+i = 0
+avg_mark = 0
+while i < len(student_marks):
+#    print(type(avg_mark), type(student_marks[i]))
+#    avg_mark = avg_mark + int(student_marks [i])
+    avg_mark += int(student_marks[i])
+#    i = i + 1
+    i += 1
+print(avg_mark)
+
+# avg_mark = avg_mark / len(student_marks)
+avg_mark /= len(student_marks)
+print('Средний балл: ', avg_mark)
+
+# <num1> + <num2> - binary operation
+# -<num> - unary operation
