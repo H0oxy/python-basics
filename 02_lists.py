74f4378688f84b62987441dc66e85ea13fd2dfda
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-09-14 13:07:17 +0500

    commit

--- 02_lists.py
+++ 02_lists.py
@@ -1,4 +1,5 @@
-lesson_dates = ['19.05.15',
+lesson_dates = [
+                 '19.05.15',
                  '19.05.17',
                  '19.05.18',
                  '19.05.19',
@@ -21,5 +22,5 @@ student_marks_2 = [
                  3
 ]
 
-for lesson_dates, mark id zip(lesson_dates, student_marks, student_marks_2): # ['19.05.15', 5]
+for lesson_dates, mark id zip(lesson_dates, student_marks): # ['19.05.15', 5]
     print(lesson_dates, 'оценка', mark)
---
+++ 03_lists.py
@@ -0,0 +1,26 @@
+lesson_dates = [
+                 '19.05.15',
+                 '19.05.17',
+                 '19.05.18',
+                 '19.05.19',
+                 '19.05.20',
+]
+
+student_marks = [
+                 5,
+                 4,
+                 3,
+                 2,
+                 5
+]
+
+student_marks_2 = [
+                 4,
+                 3,
+                 5,
+                 5,
+                 3
+]
+
+for lesson_dates, mark id zip(lesson_dates, student_marks): # ['19.05.15', 5]
+    print(lesson_dates, 'оценка', mark)
