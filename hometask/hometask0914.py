6a6cbfe2b3aea86f16eb32149624c5177466ff31
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-09-27 19:18:47 +0500

    homework0914

---
+++ hometask/hometask0914.py
@@ -0,0 +1,24 @@
+student_marks = []
+while True:
+    mark = input('Введите оценку студента:\n')
+    if mark:
+        student_marks.append(int(mark))
+        if int(mark) > 5:
+            print('Input correct mark')
+            student_marks.remove(int(mark))
+        elif int(mark) <= 1:
+            print('Input correct mark')
+            student_marks.remove(int(mark))
+    else:
+        break
+        print('Ввод завершен')
+
+avg_mark = 0
+for marks in student_marks:
+    avg_mark += marks
+avg_mark /= len(student_marks)
+if not student_marks:
+    print('')
+else:
+    print('Оценки:', student_marks)
+    print('Средний балл:', avg_mark)
