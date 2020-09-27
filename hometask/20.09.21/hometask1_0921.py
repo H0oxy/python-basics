842ca4bdc4e30da25b79e66ee68f82d1f5f9b6da
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-09-27 20:36:36 +0500

    homework0921

--- hometask/20.09.21/hometask1_0921.py
+++ hometask/20.09.21/hometask1_0921.py
@@ -1,4 +1,4 @@
 row_record = '217.168.17.5 - - [ 17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
 r1 = row_record.split() [0]
 r2 = row_record.split() [4]
-print(r1,",",r2)
+print('IP address:',r1,'\n' 'Datatime:',r2)
