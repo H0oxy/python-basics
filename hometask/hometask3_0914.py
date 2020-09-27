246482d098119309cbeb32fbe870bceb331aa167
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-09-27 19:21:49 +0500

    homework0914

---
+++ hometask/hometask3_0914.py
@@ -0,0 +1,27 @@
+q = 'HoMeTaSk'.lower()
+print(q)
+
+w = 'HoMeTaSk'.upper()
+print(w)
+
+e = 'aHoMeTaSk'.count('a')
+print(e)
+
+r = 'aHoMeTaSk'.count('a', 6)
+print(r)
+
+t = 'aHoMeTaSk'
+print(t.find('Me'))
+
+y = 'aHoMeTaSk'
+print(y.find('minus1'))
+
+u = 'a HoMe TaSk'
+print(u.replace('',' '))
+
+u1 = 'a HoMe TaSk'
+print(u1.replace(' ',''))
+
+print('input.upper')
+p1 = input().upper()
+print(p1)
